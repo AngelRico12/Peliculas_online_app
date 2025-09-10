5a95643111d27c10f87b0ba9297f5edd4b63ba57
AngelRico12 <angelrico122001@gmail.com>
2025-09-10 13:41:45 -0600

    Implementar feature funcionalidad de crear peliculas

--- src/paginas/Home.jsx
+++ src/paginas/Home.jsx
@@ -2,10 +2,10 @@ export default function Home() {
 return (
 <section className="card">
 <h2>Bienvenido/a</h2>
-<p>Esta SPA permite **crear, listar, editar y eliminar** cursos usando almacenamiento local como backend simulado.</p>
+<p>Esta SPA permite **crear, listar, editar y eliminar** peliculas usando una conexion a postgres.</p>
 <ul>
 <li>SPA con React y React Router</li>
-<li>CRUD completo de cursos</li>
+<li>CRUD completo de peliculas</li>
 <li>Git, ramas, PRs y CI con GitHub Actions</li>
 </ul>
 </section>
